a4aae030ded430f01c8421d638f2ac808499fda0
wangnaipan <783207481@qq.com>
2016-08-29 11:21:58 +0800

    mtime movie 1

---
+++ MTime/app/src/main/java/com/johnwharton/mtime/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.johnwharton.mtime.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.johnwharton.mtime.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
